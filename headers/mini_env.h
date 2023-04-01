6c7550cc9d81fd0b0cdfde2785c69cf58e373850
Heeoh Son <heson@cx2r2s10.42seoul.kr>
2023-04-01 19:01:48 +0900

    norm header

--- headers/mini_env.h
+++ headers/mini_env.h
@@ -6,7 +6,7 @@
 /*   By: heson <heson@Student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:40:06 by heson             #+#    #+#             */
-/*   Updated: 2023/03/30 15:46:01 by heson            ###   ########.fr       */
+/*   Updated: 2023/04/01 18:51:43 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,35 @@
 # define MINI_ENV_H
 
 # include "../library/libft/libft.h"
-# include "../headers/mini_utils.h"
-
-extern int	g_exit_status;
+# include "mini_utils.h"
 
 typedef struct s_env_var {
-	char    *key;
-	char    *value;
+	char	*key;
+	char	*value;
 	int		is_tmp;
-}   t_env_var;
+}	t_env_var;
 
+// env
+char		*ft_getenv(t_list *env_lst, char *key);
+int			ft_putenv(t_list *env_lst, char *arg);
+char		*replace_env(t_list *env_lst, char *data);
+
+// env_var
 t_env_var	*create_env_var(char *arg);
 t_env_var	*create_env_var_struct(char *key, char *val, int is_tmp);
 void		free_env_var(void	*arg);
 void		*copy_env_var(void *arg);
 
-char		*ft_getenv(t_list *env_lst, char *key);
-int			ft_putenv(t_list *env_lst, char *arg);
-char		*replace_env(t_list *env_lst, char *data);
-
+// env_lst
 t_list		*init_env_lst(char *org_env[]);
 void		sort_env_lst(t_list **env_lst);
 void		print_env_lst(t_list *env_lst, int env_flag);
 char		**envlst_2_arr(t_list *env_lst);
 
-
-
+// env_utils
+int			is_var_char(char ch);
+int			is_valid_key(char *key);
+void		split_lst(t_list *source, t_list **front, t_list **back);
+t_list		*merge(t_list *a, t_list *b);
 
 #endif
--- headers/mini_exe.h
+++ headers/mini_exe.h
@@ -6,27 +6,48 @@
 /*   By: heson <heson@Student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 17:01:23 by heson             #+#    #+#             */
-/*   Updated: 2023/03/31 17:10:12 by heson            ###   ########.fr       */
+/*   Updated: 2023/04/01 18:52:01 by heson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_EXE_H
 # define MINI_EXE_H
 
-# include "../headers/minishell.h"
+# include "minishell.h"
 # include "../library/get_next_line/get_next_line.h"
-# include <unistd.h> // access
-# include <stdio.h> // printf 
-# include <fcntl.h> // open, write
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
 
 # define PIPE_N 2
 # define STD 2
 
 void	execute(int cmd_cnt, t_list *cmd_p, t_list *env);
 
-char	*find_path(char *cmd, t_list *env);
-int	do_redirection_in(char *val, int *fd, char	is_heredoc);
+// cmd
+int		find_cmd_path(char *cmd, t_list *env, char **path);
+int		is_built_in(char *cmd);
+
+// exe_utils
+void	ft_free_2d_arr(char **arr);
+void	set_exit_status(int status);
+void	set_child_exe(int fd_std[], int heredoc_fd);
+void	close_fds(int fds[][2]);
+
+// redirection
+int		do_redirection(int type, char *val, int *fd);
+int		do_redirection_in(char *val, int *fd, char is_heredoc);
 int		do_redirection_out(char *filename, int *fd, char is_append);
-int     do_heredoc(char *limiter, int *input_fd, int fd_std[]);
+int		check_n_do_heredoc(t_list *rd_p, int fd_std[], int *fd);
+int		do_heredoc(char *limiter, int *input_fd, int fd_std[]);
+
+// rd_utils
+int		get_heredoc_input(int fd[], int fd_std[], char *limiter);
+int		wait_heredoc_input(int fd[], pid_t child_pid);
+
+// pipe
+int		child_process(int cmd_i, int cmd_cnt, int pipes[][2], int is_heredoc);
+void	parent_process(int cmd_i, int pipes[][2]);
+int		wait_processes(int child_cnt, pid_t last_pid, pid_t wait_pid);
 
 #endif
